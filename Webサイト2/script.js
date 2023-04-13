828c1abced2b637750f35c6c0551d7734fbbba6a
Tsora1216 <so12ra16@icloud.com>
2023-04-13 22:26:24 +0900

    update

--- Webサイト2/script.js
+++ Webサイト2/script.js
@@ -1,56 +1,75 @@
-// CSVファイルの読み込み
-fetch('output.csv')
-  .then(response => response.text())
-  .then(text => {
-    const csv = text.split('\n').slice(0, -1);
-    const data = csv.map(line => {
-      const [name, image] = line.split(',');
-      return { name, image };
-    });
+// 問題画面の要素を取得
+const quizScreen = document.getElementById('quiz-screen');
+const quizImage = document.getElementById('quiz-image');
+const answerInput = document.getElementById('answer-input');
+const answerButton = document.getElementById('answer-button');
 
-    // Start画面のスタートボタンを押したら、問題画面に遷移する処理
-    const startButton = document.getElementById('start-button');
-    const quizScreen = document.getElementById('quiz-screen');
-    startButton.addEventListener('click', () => {
-      quizScreen.style.display = 'block';
-      startButton.parentNode.style.display = 'none';
+// 結果画面の要素を取得
+const resultScreen = document.getElementById('result-screen');
+const feedback = document.getElementById('feedback');
+const correctAnswer = document.getElementById('correct-answer');
+const nextButton = document.getElementById('next-button');
 
-      // ランダムに人物を選択して、問題画面に表示する処理
-      const index = Math.floor(Math.random() * data.length);
-      const quizImage = document.getElementById('quiz-image');
-      quizImage.src = data[index].image;
-      const correctAnswer = data[index].name;
-      
-      // 回答ボタンを押したら、正解か不正解かを判定し、結果画面に遷移する処理
-      const answerButton = document.getElementById('answer-button');
-      const answerInput = document.getElementById('answer-input');
-      answerButton.addEventListener('click', () => {
-        const answer = answerInput.value;
-        const feedback = document.getElementById('feedback');
-        const correctAnswerDiv = document.getElementById('correct-answer');
-        if (answer === correctAnswer) {
-          feedback.textContent = '正解';
-          correctAnswerDiv.style.display = 'none';
-        } else {
-          feedback.textContent = '不正解';
-          correctAnswerDiv.style.display = 'block';
-          correctAnswerDiv.textContent = `正解：${correctAnswer}`;
-        }
-        quizScreen.style.display = 'none';
-        resultScreen.style.display = 'block';
-      });
+// Start画面の要素を取得
+const startButton = document.getElementById('start-button');
+const startScreen = document.getElementById('start-screen');
 
-      // 次の問題へボタンを押したら、新しい問題を表示する処理
-      const nextButton = document.getElementById('next-button');
-      const resultScreen = document.getElementById('result-screen');
-      nextButton.addEventListener('click', () => {
-        resultScreen.style.display = 'none';
-        startButton.parentNode.style.display = 'none';
-        quizScreen.style.display = 'block';
-        const index = Math.floor(Math.random() * data.length);
-        quizImage.src = data[index].image;
-        correctAnswer = data[index].name;
-        answerInput.value = '';
-      });
-    });
-  });
+// CSVファイルのURL
+const csvUrl = 'output.csv';
+
+// CSVファイルを取得して解析する
+const fetchCsv = async (url) => {
+  const response = await fetch(url);
+  const data = await response.text();
+  const lines = data.split('\n').map(line => line.split(','));
+  return lines;
+};
+
+// CSVファイルからランダムに問題を選択する
+const selectRandomQuestion = async () => {
+  const csvData = await fetchCsv(csvUrl);
+  const randomIndex = Math.floor(Math.random() * csvData.length);
+  const [name, imageUrl] = csvData[randomIndex];
+  return { name, imageUrl };
+};
+
+// 問題画面に問題を表示する
+const showQuestion = async () => {
+  const { name, imageUrl } = await selectRandomQuestion();
+  quizImage.src = imageUrl;
+  answerInput.value = '';
+  quizScreen.style.display = 'block';
+  resultScreen.style.display = 'none';
+};
+
+// ユーザーが回答を送信したら、正解かどうかを判定する
+const checkAnswer = () => {
+  const answer = answerInput.value.trim().toLowerCase();
+  const name = quizImage.alt.trim().toLowerCase();
+
+  if (answer === name) {
+    feedback.textContent = '正解！';
+    correctAnswer.style.display = 'none';
+  } else {
+    feedback.textContent = '不正解';
+    correctAnswer.textContent = `正解：${name}`;
+    correctAnswer.style.display = 'block';
+  }
+
+  quizScreen.style.display = 'none';
+  resultScreen.style.display = 'block';
+};
+
+// 「次の問題へ」ボタンがクリックされたら、新しい問題を表示する
+const nextQuestion = () => {
+  showQuestion();
+};
+
+// スタートボタンがクリックされたら、最初の問題を表示する
+startButton.addEventListener('click', showQuestion);
+
+// 回答ボタンがクリックされたら、正解かどうかを判定する
+answerButton.addEventListener('click', checkAnswer);
+
+// 「次の問題へ」ボタンがクリックされたら、新しい問題を表示する
+nextButton.addEventListener('click', nextQuestion);
